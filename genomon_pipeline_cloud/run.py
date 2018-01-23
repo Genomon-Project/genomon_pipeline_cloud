0f0873554004b0f27547041fb557a20541acb132
Yuichi Shiraishi <friend1ws@gmail.com>
2018-01-23 10:39:49 +0900

    add multirocessing treatments

--- genomon_pipeline_cloud/run.py
+++ genomon_pipeline_cloud/run.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys, argparse, tempfile, shutil, os, subprocess, pkg_resources
+import sys, argparse, tempfile, shutil, os, subprocess, multiprocessing, pkg_resources
 from ConfigParser import SafeConfigParser
 from batch_engine import *
 
@@ -35,13 +35,27 @@ def run(args):
     # RNA
     from tasks.star_alignment import *
     from tasks.fusionfusion import *    
+    from tasks.genomon_expression import *
 
     star_alignment_task = Star_alignment(args.output_dir, tmp_dir, sample_conf, param_conf)
     fusionfusion_task = Fusionfusion(args.output_dir, tmp_dir, sample_conf, param_conf)
+    genomon_expression_task = Genomon_expression(args.output_dir, tmp_dir, sample_conf, param_conf)
 
-    batch_engine.execute(star_alignment_task)
-    batch_engine.execute(fusionfusion_task)  
+    # first stage    
+    p1 = multiprocessing.Process(target = batch_engine.execute, args = (star_alignment_task,))
+    p1.start()
+    p1.join()
+
+    # second stage
+    p2 = multiprocessing.Process(target = batch_engine.execute, args = (fusionfusion_task,))
+    p3 = multiprocessing.Process(target = batch_engine.execute, args = (genomon_expression_task,))
+    p2.start()
+    p3.start()
+
+    p2.join()
+    p3.join()
+    
 
     # remove the temporary directory
-    shutil.rmtree(tmp_dir_name)
+    # shutil.rmtree(tmp_dir)
 
