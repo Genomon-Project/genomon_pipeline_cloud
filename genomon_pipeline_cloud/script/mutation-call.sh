7a7cc12144a07adae21161fcc974de059f899d11
ken0-1n <ubuntu@ip-172-31-34-116.ap-northeast-1.compute.internal>
2020-03-02 09:29:48 +0000

    update mutation-call

--- genomon_pipeline_cloud/script/mutation-call.sh
+++ genomon_pipeline_cloud/script/mutation-call.sh
@@ -44,12 +44,16 @@ else
     fi
     fisher comparison -o ${OUTPUT_PREF}.fisher_mutations.txt --ref_fa ${REFERENCE} -1 ${INPUT_BAM1} -2 ${INPUT_BAM2} --samtools_path ${SAMTOOLS} ${FISHER_PAIR_OPTION} "${FISHER_PAIR_SAMTOOLS}"
     
-    # Identifying mutations in cancer hotspot
-    if [ "_${HOTSPOT_SAMTOOLS}" != "_" ]; then
-        HOTSPOT_OPTION="${HOTSPOT_OPTION} -S "
+    if [ "_${HOTSPOT_DB}" != "_" ]; then
+        # Identifying mutations in cancer hotspot
+        if [ "_${HOTSPOT_SAMTOOLS}" != "_" ]; then
+            HOTSPOT_OPTION="${HOTSPOT_OPTION} -S "
+        fi
+        hotspotCall ${HOTSPOT_OPTION} "${HOTSPOT_SAMTOOLS}" ${INPUT_BAM1} ${INPUT_BAM2} ${OUTPUT_PREF}.hotspot_mutations.txt ${HOTSPOT_DB} 
+        mutil merge_hotspot -i ${OUTPUT_PREF}.hotspot_mutations.txt -f ${OUTPUT_PREF}.fisher_mutations.txt -o ${OUTPUT_PREF}.fisher_hotspot_mutations.txt --hotspot_header
+    else
+        cp ${OUTPUT_PREF}.fisher_mutations.txt ${OUTPUT_PREF}.fisher_hotspot_mutations.txt
     fi
-    hotspotCall ${HOTSPOT_OPTION} "${HOTSPOT_SAMTOOLS}" ${INPUT_BAM1} ${INPUT_BAM2} ${OUTPUT_PREF}.hotspot_mutations.txt ${HOTSPOT_DB} 
-    mutil merge_hotspot -i ${OUTPUT_PREF}.hotspot_mutations.txt -f ${OUTPUT_PREF}.fisher_mutations.txt -o ${OUTPUT_PREF}.fisher_hotspot_mutations.txt --hotspot_header
     
     # Local realignment using blat. The candidate mutations are varidated.
     mutfilter realignment --target_mutation_file ${OUTPUT_PREF}.fisher_hotspot_mutations.txt -1 ${INPUT_BAM1} -2 ${INPUT_BAM2} --output ${OUTPUT_PREF}.realignment_mutations.txt --ref_genome ${REFERENCE} --blat_path ${BLAT} ${REALIGNMENT_OPTION}
@@ -88,7 +92,11 @@ if [ _${SAMPLE2} = "_None" ]
 then
     mut_header="Chr,Start,End,Ref,Alt,depth,variantNum,bases,A_C_G_T,misRate,strandRatio,10%_posterior_quantile,posterior_mean,90%_posterior_quantile,readPairNum,variantPairNum,otherPairNum,10%_posterior_quantile(realignment),posterior_mean(realignment),90%_posterior_quantile(realignment),simple_repeat_pos,simple_repeat_seq,P-value(EBCall)"
 else
-    mut_header="Chr,Start,End,Ref,Alt,depth_tumor,variantNum_tumor,depth_normal,variantNum_normal,bases_tumor,bases_normal,A_C_G_T_tumor,A_C_G_T_normal,misRate_tumor,strandRatio_tumor,misRate_normal,strandRatio_normal,P-value(fisher),score(hotspot),readPairNum_tumor,variantPairNum_tumor,otherPairNum_tumor,readPairNum_normal,variantPairNum_normal,otherPairNum_normal,P-value(fisher_realignment),indel_mismatch_count,indel_mismatch_rate,bp_mismatch_count,distance_from_breakpoint,simple_repeat_pos,simple_repeat_seq,P-value(EBCall)"
+    if [ "_${HOTSPOT_DB}" != "_" ]; then
+        mut_header="Chr,Start,End,Ref,Alt,depth_tumor,variantNum_tumor,depth_normal,variantNum_normal,bases_tumor,bases_normal,A_C_G_T_tumor,A_C_G_T_normal,misRate_tumor,strandRatio_tumor,misRate_normal,strandRatio_normal,P-value(fisher),score(hotspot),readPairNum_tumor,variantPairNum_tumor,otherPairNum_tumor,readPairNum_normal,variantPairNum_normal,otherPairNum_normal,P-value(fisher_realignment),indel_mismatch_count,indel_mismatch_rate,bp_mismatch_count,distance_from_breakpoint,simple_repeat_pos,simple_repeat_seq,P-value(EBCall)"
+    else
+        mut_header="Chr,Start,End,Ref,Alt,depth_tumor,variantNum_tumor,depth_normal,variantNum_normal,bases_tumor,bases_normal,A_C_G_T_tumor,A_C_G_T_normal,misRate_tumor,strandRatio_tumor,misRate_normal,strandRatio_normal,P-value(fisher),readPairNum_tumor,variantPairNum_tumor,otherPairNum_tumor,readPairNum_normal,variantPairNum_normal,otherPairNum_normal,P-value(fisher_realignment),indel_mismatch_count,indel_mismatch_rate,bp_mismatch_count,distance_from_breakpoint,simple_repeat_pos,simple_repeat_seq,P-value(EBCall)"
+    fi
 fi
 
 if [ _${CONTROL_BAM_LIST} = "_None" ]
@@ -121,7 +129,11 @@ if [ _${SAMPLE2} = "_None" ]
 then 
     mutil filter -i ${OUTPUT_PREF}.genomon_mutation.result.txt -o ${OUTPUT_PREF}.genomon_mutation.result.filt.txt ${FILTER_SINGLE_OPTION}
 else
-    mutil filter -i ${OUTPUT_PREF}.genomon_mutation.result.txt -o ${OUTPUT_PREF}.genomon_mutation.result.filt.txt ${FILTER_PAIR_OPTION} --hotspot_db ${HOTSPOT_DB}
+    if [ "_${HOTSPOT_DB}" != "_" ]; then
+        mutil filter -i ${OUTPUT_PREF}.genomon_mutation.result.txt -o ${OUTPUT_PREF}.genomon_mutation.result.filt.txt ${FILTER_PAIR_OPTION} --hotspot_db ${HOTSPOT_DB}
+    else
+        mutil filter -i ${OUTPUT_PREF}.genomon_mutation.result.txt -o ${OUTPUT_PREF}.genomon_mutation.result.filt.txt ${FILTER_PAIR_OPTION}
+    fi
 fi
 
 rm -f ${OUTPUT_PREF}.ExAC.txt
